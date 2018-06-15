de7dedaad27a9a8a566856c7a310d8f2e82d48b5
fvendrameto <fernandovendrameto@gmail.com>
2018-06-15 09:04:41 -0300

    UPLOAD PORRA

--- course/views.py
+++ course/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
+from django.core.exceptions import ObjectDoesNotExist
 
-from course.forms import CourseForm
-from course.models import Course
+from course.forms import CourseForm, LinkForm, ListForm, SummaryForm
+from course.models import Course, Link, List, Summary
 
 def course_list(request):
 	courses = Course.objects.all()
@@ -12,18 +12,21 @@ def course_list(request):
 
 def course_details(request, pk):
 	course = Course.objects.get(pk=pk)
-	return render(request, 'course/detail.html', {'course':course})
+	context = {'course':course}	
+	return render(request, 'course/detail.html', context)
 
 def course_new(request):
 	if request.method == "POST":
-		form = CourseForm(request.POST)
+		form = CourseForm(request.POST)		
 		if form.is_valid():
 			course = form.save(commit=False)
 			course.save()	
 			return redirect('course_details', pk=course.pk)
 	else:
 		form = CourseForm()
-	return render(request, 'course/new.html', {'form':form})
+
+	context = {'form': form}
+	return render(request, 'course/new.html', context)
 
 def course_edit(request, pk):
 	course = Course.objects.get(pk=pk)
@@ -31,15 +34,55 @@ def course_edit(request, pk):
 	
 	if request.method == "POST":
 		if form.is_valid():
-			course = form.save(commit=False)
+			course = course_form.save(commit=False)
 			course.save()
+			handle_uploaded_file(request.FILES['file'])
 			return redirect('course_details', pk=course.pk)
 		else:
 			form = CourseForm()
-	context = {'form' : form, 'course' : course}
+
+	context = {'form': form, 'course': course}
 	return render(request, 'course/new.html', context)
 
 def course_remove(request, pk):
 	course = Course.objects.get(pk=pk)
 	course.delete()
 	return redirect('course_list')
+
+def link_add(request):
+	if request.method == "POST":
+		form = LinkForm(request.POST)
+		if form.is_valid():
+			link = form.save(commit=False)
+			link.save()
+			return redirect('course_list')
+	else:
+		form = LinkForm()
+	context = {'form' : form}
+	return render(request, 'course/new.html', context)
+
+def list_add(request):
+	if request.method == "POST":
+		form = ListForm(request.POST, request.FILES)
+		if form.is_valid():
+			list_new = form.save(commit=False)
+			list_new.save()
+
+			return redirect('course_list')
+	else:
+		form = ListForm()
+	context = {'form' : form}
+	
+	return render(request, 'course/new.html', context)
+
+def summary_add(request):
+	if request.method == "POST":
+		form = SummaryForm(request.POST)
+		if form.is_valid():
+			summary = form.save(commit=False)
+			summary.save()
+			return redirect('course_list')
+	else:
+		form = SummaryForm()
+	context = {'form' : form}
+	return render(request, 'course/new.html', context)
