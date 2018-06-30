83cf49614bbca9d51f9ff147fd8d0d6e7bdae25a
fvendrameto <fernandovendrameto@gmail.com>
2018-06-29 23:48:32 -0300

    seila

--- course/views.py
+++ course/views.py
@@ -30,7 +30,6 @@ def course_new(request):
 def course_edit(request, pk):
 	course = Course.objects.get(pk=pk)
 	form = CourseForm(instance=course)
-	
 	if request.method == "POST":
 		if form.is_valid():
 			course = course_form.save(commit=False)
@@ -70,6 +69,26 @@ def link_detail(request, pk):
 	context = {'link':link}
 	return render(request, 'course/detail.html', context)
 
+def link_edit(request, pk):
+	link = Link.objects.get(pk=pk)
+	form = LinkForm(instance=link)
+	
+	if request.method == "POST":
+		if form.is_valid():
+			link = form.save(commit=False)
+			link.save()
+			return redirect('link_detail', pk=pk)
+		else:
+			form = LinkForm()
+	context = {'form': form, 'link': link}
+	return render(request, 'course/new.html', context)
+
+def link_remove(request, pk):
+	link = Link.objects.get(pk=pk)
+	pk = link.course
+	link.delete()
+	return redirect('course_detail', pk)
+
 def list_add(request):
 	if request.method == "POST":
 		form = ListForm(request.POST, request.FILES)
@@ -96,6 +115,26 @@ def list_detail(request, pk):
 	context = {'list':list_c}
 	return render(request, 'course/detail.html', context)
 
+def list_edit(request, pk):
+	list_c = List.objects.get(pk=pk)
+	form = ListForm(instance=list_c)
+
+	if request.method == "POST":
+		if form.is_valid():
+			list_c = form.save(commit=False)
+			list_c.save()
+			return redirect('list_detail', pk=pk)
+	else:
+		form = LinkForm()
+	context = {'form': form, 'list': list_c}
+	return render(request, 'course/new.html', context)
+
+def list_remove(request, pk):
+	list_c = List.objects.get(pk=pk)
+	pk = list_c.course
+	list_c.delete()
+	return redirect('course_detail', pk)
+
 def summary_add(request):
 	if request.method == "POST":
 		form = SummaryForm(request.POST, request.FILES)
@@ -105,8 +144,8 @@ def summary_add(request):
 			summary.tags.set(form.cleaned_data['tags'])
 
 			return redirect('course_list')
-	else:
-		form = SummaryForm()
+		else:
+			form = SummaryForm()
 	context = {'form' : form}
 	return render(request, 'course/new.html', context)
 
@@ -120,3 +159,22 @@ def summary_detail(request, pk):
 	summary = Summary.objects.get(pk=pk)
 	context = {'summary':summary}
 	return render(request, 'course/detail.html', context)
+
+def summary_edit(request, pk):
+	summary = Summary.objects.get(pk=pk)
+	form = SummaryForm(instance=summary)
+	if request.method == "POST":
+		if form.is_valid():
+			summary = form.save(commit=False)
+			summary.save()
+			return redirect('summary_detail', pk=pk)
+		else:
+			form = LinkForm()
+	context = {'form': form, 'summary': summary}
+	return render(request, 'course/new.html', context)
+
+def summary_remove(request, pk):
+	summary = Summary.objects.get(pk=pk)
+	pk = summary.course
+	summary.delete()
+	return redirect('course_detail', pk)
