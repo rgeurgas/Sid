de8102598dba82f1cafda821fd319ec3da983841
Rodrigo Geurgas <rodrigogeurgas@gmail.com>
2018-07-02 00:32:37 -0300

    maybe funcionou o url, não consigo testar

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import re_path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 from course import views
 
@@ -25,5 +26,4 @@ urlpatterns = [
     re_path(r'^auth/', include('registration.urls'), name='auth'),
     re_path(r'^forum/', include('forum.urls'), name='forum'),
     re_path(r'^media/(?P<path>uploads/[0-9]{4}/[0-9]{2}/[0-9]{2}/.{1,100})', views.download, name='downloads'),
-    
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
