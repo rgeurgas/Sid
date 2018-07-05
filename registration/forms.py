27b1b69344d16d81a3b178626ad2ea1482c13fb5
Guilherme <guimontemovo@gmail.com>
2018-07-05 13:03:19 -0300

    bio singup fixed

--- registration/forms.py
+++ registration/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class SignUpForm(UserCreationForm):
 	birth_date = forms.DateField(widget=forms.DateInput(attrs={'id':'birthdate', 'class':'datepicker'}))
 	university = forms.CharField(max_length=50)
-	bio = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'id':'id_bio'}))
+	bio = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'id':'id_bio', 'class':'materialize-textarea'}))
 	email = forms.EmailField(max_length=254, help_text='Required. Please insert a valid email')
 
 	class Meta:
